5f5459fa05311ab79811e1ee382f3052b114f4e9
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-06-05 14:47:23 +0300

    deleted Quotas

--- Quotas/src/config/kafka.ts
+++
@@ -1,9 +0,0 @@
-import { Kafka } from "kafkajs"
-
-const kafka = new Kafka({
-   clientId: "consumer",
-   brokers: ["10.31.5.74:9092"] // [env.get("KAFKA_BOOTSTRAP_SERVER").required().asString()],
-})
-
-
-export default kafka;
