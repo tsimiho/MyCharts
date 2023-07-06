9a4746a629ff0bb19ff530b1bcfe02f7a2ddeed2
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-07-06 17:36:33 +0300

    removed socket from confirmation page

--- frontend/saas23-frontend/src/pages/ConfirmationPage.js
+++ frontend/saas23-frontend/src/pages/ConfirmationPage.js
@@ -15,28 +15,7 @@ function Confirmation({
         setUserdata({});
     };
 
-    useEffect(() => {
-        const socket = new WebSocket("ws://localhost:8090");
-
-        socket.onopen = () => {
-            console.log("WebSocket connection established");
-        };
-
-        socket.onclose = () => {
-            console.log("WebSocket connection closed");
-        };
-
-        socket.onmessage = ({ data }) => {
-            data = JSON.parse(data);
-            console.log(data);
-            setUserdata(data);
-            localStorage.setItem("userdata", JSON.stringify(data));
-        };
-
-        return () => {
-            socket.close();
-        };
-    }, []);
+    useEffect(() => {}, []);
 
     return (
         <div className="background">
