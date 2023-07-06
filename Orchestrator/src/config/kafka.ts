0707b8699c5e8f257cd81d85bd92a048071cf532
ntua-el19045 <el19045@mail.ntua.gr>
2023-07-07 01:44:45 +0300

    changed localhost:9092 to kafka:9092

--- Orchestrator/src/config/kafka.ts
+++ Orchestrator/src/config/kafka.ts
@@ -2,7 +2,7 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
     clientId: "producer",
-    brokers: ["localhost:9092"], // [env.get("KAFKA_BOOTSTRAP_SERVER").required().asString()],
+    brokers: ["kafka:9092"], // [env.get("KAFKA_BOOTSTRAP_SERVER").required().asString()],
 });
 
 export default kafka;
